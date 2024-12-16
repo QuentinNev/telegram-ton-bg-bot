b3cbf9f7bbd459414666de2647f32c4e2175039f
QuentinNev <neves.quentin@gmail.com>
2024-12-16 11:11:30 +0100

    Fix commands

--- src/bot/commands/index.ts
+++ src/bot/commands/index.ts
@@ -2,12 +2,15 @@ import { Bot, Context } from 'grammy';
 import { readdirSync } from 'fs';
 import { join } from 'path';
 
-export const loadCommands = (bot: Bot<Context>) => {
+export const loadCommands = async (bot: Bot<Context>) => {
   const commandsPath = join(__dirname);  // Path to the commands folder
   const commands: any[] = [];
 
   // Read all files in the commands folder
-  readdirSync(commandsPath).forEach((file) => {
+  const files = readdirSync(commandsPath);
+
+  for (let i = 0; i < files.length; i++) {
+    const file = files[i];
     if (file !== 'index.ts') {
       const commandName = file.split('.')[0];  // Get the file name without extension
 
@@ -16,21 +19,19 @@ export const loadCommands = (bot: Bot<Context>) => {
       const fileURL = `file://${filePath}`;
 
       // Import each command dynamically using the file URL
-      import(fileURL).then((commandModule) => {
+      await import(fileURL).then((commandModule) => {
         const { description, command } = commandModule;
 
-        // Register the command description
-        if (description && command) {
+        if (command && description) {
           commands.push({ command: commandName, description });
-
-          // Execute the command function
           bot.command(commandName, command);
         }
       }).catch((error) => {
         console.error(`Error loading command ${file}:`, error);
       });
     }
-  });
+  }
 
+  console.log(commands)
   bot.api.setMyCommands(commands);
 };
