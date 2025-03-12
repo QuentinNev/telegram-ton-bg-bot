8c04a89e890762904bfc0dfefa3f1d67f78b00ae
QuentinNev <neves.quentin@gmail.com>
2025-03-12 15:46:32 +0100

    Make command accept any as return value

--- src/bot/types/command.ts
+++ src/bot/types/command.ts
@@ -3,12 +3,12 @@ import { Context } from "grammy";
 /**
  * @typedef {Object} Command A Telegram bot command
  * @property {string} description - A brief description of the command.
- * @property {(ctx: Context) => void | Promise<void>} command - The function the command will execute. 
+ * @property {(ctx: Context) => any | Promise<any>} command - The function the command will execute. 
  * @property {boolean} public - Indicates if the command is publicly accessible.
  */
 type Command = {
     description: string;
-    command: (ctx: Context) => void | Promise<void>;
+    command: (ctx: Context) => any | Promise<any>;
     public: boolean
 }
 
