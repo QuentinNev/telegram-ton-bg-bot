0b127259213ef6a9afcc04420ff7af691ccc9563
QuentinNev <neves.quentin@gmail.com>
2025-03-12 15:44:47 +0100

    Convert ban commands to export Command type

--- src/bot/commands/hardban.ts
+++ src/bot/commands/hardban.ts
@@ -1,17 +1,23 @@
 import "dotenv/config.js";
 import { Context } from "grammy";
 import isModerator from "../utils/modertators";
+import Command from "../types/command";
 
-export const description: string = "Hard ban a user";
-export const command = async (ctx: Context) => {
-    if (!await isModerator(ctx)) return;
-    if (!ctx.message?.reply_to_message) return;
-    const target = {
-        id: ctx.message.reply_to_message.from?.id,
-        username: ctx.message.reply_to_message.from?.username
+const command: Command = {
+    public: false,
+    description: "Hard ban a user",
+    command: async (ctx: Context) => {
+        if (!await isModerator(ctx)) return;
+        if (!ctx.message?.reply_to_message) return;
+        const target = {
+            id: ctx.message.reply_to_message.from?.id,
+            username: ctx.message.reply_to_message.from?.username
+        }
+
+        // TODO : Hard ban user
+
+        await ctx.reply(`Hard banned ${target.username}`);
     }
-
-    // TODO : Hard ban user
-
-    await ctx.reply(`Hard banned ${target.username}`);
 }
+
+export default command;
--- src/bot/commands/unban.ts
+++ src/bot/commands/unban.ts
@@ -1,17 +1,23 @@
 import "dotenv/config.js";
 import { Context } from "grammy";
 import isModerator from "../utils/modertators";
+import Command from "../types/command";
 
-export const description: string = "Unban a user";
-export const command = async (ctx: Context) => {
-    if (!await isModerator(ctx)) return;
-    if (!ctx.message?.reply_to_message) return;
-    const target = {
-        id: ctx.message.reply_to_message.from?.id,
-        username: ctx.message.reply_to_message.from?.username
+const command: Command = {
+    public: false,
+    description: "Unban a user",
+    command: async (ctx: Context) => {
+        if (!await isModerator(ctx)) return;
+        if (!ctx.message?.reply_to_message) return;
+        const target = {
+            id: ctx.message.reply_to_message.from?.id,
+            username: ctx.message.reply_to_message.from?.username
+        }
+
+        // TODO : Unban user
+
+        await ctx.reply(`Unbanned ${target.username}`);
     }
-
-    // TODO : Unban user
-
-    await ctx.reply(`Unbanned ${target.username}`);
 }
+
+export default command;
