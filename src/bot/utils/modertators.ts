b27166f3070dadec30355e7f6130e00e037eadca
QuentinNev <neves.quentin@gmail.com>
2025-03-12 14:59:50 +0100

    Fix exports

--- src/bot/utils/modertators.ts
+++ src/bot/utils/modertators.ts
@@ -1,20 +1,21 @@
 import { Context } from "grammy";
+import { ChatMemberAdministrator, ChatMemberOwner } from "grammy/types";
 
 /**
  * Return a list of all moderators. (In telegram, there's only normal user, admins and admins with less privileges)
  * @param ctx Context of the command
- * @returns list of all moderators
+ * @returns {Promise<(ChatMemberOwner | ChatMemberAdministrator)[]>} list of all moderators
  */
-export async function getModerators(ctx: Context) {
+export async function getModerators(ctx: Context): Promise<(ChatMemberOwner | ChatMemberAdministrator)[]> {
     return await ctx.getChatAdministrators();
 }
 
 /**
  * Check if user is a moderator
  * @param ctx Context of the command
- * @returns 
+ * @returns {Promise<boolean>} if user is moderator
  */
-export default async function isModerator(ctx: Context) {
+export default async function isModerator(ctx: Context): Promise<boolean> {
     const mods = await getModerators(ctx);
     return mods.some(mod => mod.user.id == ctx.from?.id);
 }
