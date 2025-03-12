5e2e6100dce81f2f7b744a5b7f34e91115487c2c
QuentinNev <neves.quentin@gmail.com>
2025-03-12 15:41:47 +0100

    Update command type

--- src/bot/types/command.ts
+++ src/bot/types/command.ts
@@ -1,12 +1,14 @@
+import { Context } from "grammy";
+
 /**
  * @typedef {Object} Command A Telegram bot command
  * @property {string} description - A brief description of the command.
- * @property {() => void} function - The function the command will execute. 
+ * @property {(ctx: Context) => void | Promise<void>} command - The function the command will execute. 
  * @property {boolean} public - Indicates if the command is publicly accessible.
  */
 type Command = {
     description: string;
-    function: () => void;
+    command: (ctx: Context) => void | Promise<void>;
     public: boolean
 }
 
