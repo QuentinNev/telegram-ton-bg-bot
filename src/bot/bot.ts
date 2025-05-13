ffdf54e691cfacea25ba21afeb53f3c3e781349d
QuentinNev <neves.quentin@gmail.com>
2025-05-13 11:29:28 +0200

    Set user's inTGGroup value on join, leave or msg

--- src/bot/bot.ts
+++ src/bot/bot.ts
@@ -8,6 +8,7 @@ import getRandomPhoto from "./utils/getRandomPhoto";
 import { CronJob } from "cron";
 
 const bot = new Bot<Context>(process.env.TELEGRAM_BOT_TOKEN || ``);
+const apiToken = process.env.SW_API_TOKEN || ``;
 
 loadCommands(bot);
 
@@ -15,6 +16,65 @@ const handleGracefulShutdown = async () => {
     await bot.stop();
     process.exit();
 };
+
+bot.on("message:text", async (ctx) => {
+    await fetch(`https://data.tonbg.com/users/${ctx.from.id}`, {
+        method: 'PATCH',
+        headers: {
+            'Context-Type': 'application/json',
+            'x-api-token': apiToken
+        },
+        body: JSON.stringify({ inTG: true })
+    });
+    console.log(`added ${ctx.from.first_name} to inTG!`)
+});
+
+bot.on("message:new_chat_members", async (ctx) => {
+    const newMembers = ctx.message.new_chat_members;
+
+    // Get existing users
+    const existing: any[] = await fetch(`https://data.tonbg.com/users?telegramId=${newMembers.join(',')}`).then(res => res.json());
+
+    for (const user of newMembers) {
+        // If user is not found in db, create it before updating (because flemme d'update la POST route)
+        if (!existing.some(ex => ex.id === user.id)) {
+            await fetch(`https://data.tonbg.com/users/${user.id}`, {
+                method: 'POST',
+                headers: {
+                    'Context-Type': 'application/json',
+                    'x-api-token': apiToken
+                },
+                body: JSON.stringify({
+                    username: user.username,
+                    nickname: user.first_name
+                })
+            });
+        }
+
+        await fetch(`https://data.tonbg.com/users/${user.id}`, {
+            method: 'PATCH',
+            headers: {
+                'Context-Type': 'application/json',
+                'x-api-token': apiToken
+            },
+            body: JSON.stringify({ inTG: true })
+        });
+    }
+});
+
+bot.on("message:left_chat_member", async (ctx) => {
+    const user = ctx.message.left_chat_member;
+
+    await fetch(`https://data.tonbg.com/users/${user.id}`, {
+        method: 'PATCH',
+        headers: {
+            'Context-Type': 'application/json',
+            'x-api-token': apiToken
+        },
+        body: JSON.stringify({ inTG: false })
+    });
+});
+
 
 bot.catch((e) => {
     console.log("Error", e);
